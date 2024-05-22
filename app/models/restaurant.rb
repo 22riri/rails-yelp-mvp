3bbdac8cacb633bc3f0b4ccd7b2712576b7d3fec
Ariana Detroyes <ariana.detroyes@ehl.ch>
2024-05-22 15:02:45 +0100

    rails new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,34 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  # def index
+  #   @restaurants = Restaurant.all
+  # end
+
+  # def show
+  # end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  #   @restaurant.destroy
+  # end
+
+  # private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Creating restaurants...'
+tour_d_argent = Restaurant.new(name: "La Tour d'Argent", address: "theiraddress", phone_number: "theirnumber", category: "french")
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(name: "Chez Gladines", address: "theiraddress", phone_number: "theirnumber", category: "chinese")
+chez_gladines.save!
+
+house_of_sushi = Restaurant.new(name: "House of sushi", address: "theiraddress", phone_number: "theirnumber", category: "japanese")
+house_of_sushi.save!
+
+chez_gusto = Restaurant.new(name: "Chez Gusto", address: "theiraddress", phone_number: "theirnumber", category: "italian")
+chez_gusto.save!
+
+fries_paradise = Restaurant.new(name: "Fries Paradise", address: "theiraddress", phone_number: "theirnumber", category: "belgian")
+fries_paradise.save!
+puts 'Finished!'
